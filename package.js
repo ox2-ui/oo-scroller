399e67f30d408d5aa593c730a5ceb5fdb33a78d7
Lauricio Su <me@lauricio.com>
2015-10-04 19:36:22 +0700

    updated less dependency to 2.5.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ox2:scroller',
-  summary: 'Native scroller',
-  version: '1.0.0',
+  summary: 'TESTING_DO_NOT_USE Native scroller',
+  version: '1.1.0',
   git: ' /* Fill me in! */ '
 });
 
@@ -10,15 +10,16 @@ var C = 'client';
 var CS = [C, S];
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.2.1');
+  api.versionsFrom('1.2.0.2');
   // Core
   api.use([
     'templating'
     ]);
   // 3rd party
   api.use([
-    'lauricio:less-autoprefixer@1.0.7','mquandalle:jade@0.4.1'
+    'lauricio:less-autoprefixer@2.5.0_3','mquandalle:jade@0.4.1'
     ]);
+  api.addFiles('lib/oo-color-variables.import.less', C);
   api.addFiles('lib/oo-scroller.jade', C);
   api.addFiles('lib/oo-scroller.js', C);
   api.addFiles('lib/oo-scroller.less', C);
